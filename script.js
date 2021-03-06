f2cc7487cb83f5b59932a0ba4d468235c7ce2908
unknown <iceyokuna@gmail.com>
2021-03-06 21:58:43 +0700

    Hello World!

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
+//Hello world! 3 methods
 console.log("hello world!");
+document.getElementById("text").innerHTML = "Hello World getElementById!";
+document.write("Hello World write!");
+window.alert("Hello World!");
