fe3a52f360af7a79735370424ebb6dda35adfcc3
unknown <iceyokuna@gmail.com>
2021-03-06 22:17:36 +0700

    Variable

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ var a = 1;
 var b = 2;
 var c = a + b;
 console.log(c);
+
+//function
+function myFunction(val1, val2) {
+    return val1 * val2;
+}
+var val1 = 6;
+var val2 = 12;
+console.log(myFunction(val1,val2));
+
+//document
+//tip type 'document ... in console to see specific HTML element'
+//e.g. 'document.body.style' , we will see stype attribute that we can modify
+function changeBG(){
+    document.body.style.backgroundColor = "red";
+}
