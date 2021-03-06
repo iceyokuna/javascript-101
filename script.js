0923e95ede2980e0800903d804b8a08f033e8693
unknown <iceyokuna@gmail.com>
2021-03-06 22:00:43 +0700

    Variable

--- script.js
+++ script.js
@@ -2,4 +2,10 @@
 console.log("hello world!");
 document.getElementById("text").innerHTML = "Hello World getElementById!";
 document.write("Hello World write!");
-window.alert("Hello World!");
+//window.alert("Hello World!");
+
+//Variable
+var a = 1;
+var b = 2;
+var c = a + b;
+console.log(c);
