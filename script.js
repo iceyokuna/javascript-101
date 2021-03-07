7b930437f8de441751e34f9382b524aefcadc413
unknown <iceyokuna@gmail.com>
2021-03-07 16:38:49 +0700

    init 101

--- script.js
+++ script.js
@@ -18,9 +18,36 @@ var val1 = 6;
 var val2 = 12;
 console.log(myFunction(val1,val2));
 
+//arrow function
+var hello = name => "Hello world! array function " + name;
+console.log(hello("Iceyo"));
+
 //document
 //tip type 'document ... in console to see specific HTML element'
 //e.g. 'document.body.style' , we will see stype attribute that we can modify
 function changeBG(){
     document.body.style.backgroundColor = "red";
 }
+
+//object
+var user = {
+    firstname:"Iceyo",
+    lastname:"Kuna",
+    age:"24",
+    introduce: function(){
+        return this.firstname + this.lastname;
+    }  
+};
+console.log(user.introduce());
+
+//array
+var arr1 = ['a','b'];
+var arr2 = ['c'];
+console.log(arr1+arr2);
+
+//typeof() -> check object
+//type conversion -> Number() String()
+
+
+//class
+//like function but classy
